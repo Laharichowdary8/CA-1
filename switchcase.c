daf1b4c2a92c11cdf1f0c3b1c48698617fd6a788
Patchipulusujaswitha <patchipulusu.jaswitha@s.amity.edu>
2025-03-26 14:22:39 +0530

    commit5

---
+++ switchcase.c
@@ -0,0 +1,14 @@
+switch(branch)
+	case 1:
+	printf("btech branch");
+	break;
+	case 2:
+	printf("aiml branch");
+	break;
+        
+	case 3:
+	printf("mech branch");
+	break;
+	case 4:
+	printf("biotech branch");
+	break;
